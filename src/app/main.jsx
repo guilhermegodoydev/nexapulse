cdf79c40c328c4d3c725b4ae6988bbedf2648d5f
Guilherme Godoy <guilherme.santanaa06@gmail.com>
2026-01-22 14:51:15 -0300

    implementando react query

--- src/app/main.jsx
+++ src/app/main.jsx
@@ -1,11 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
+import { QueryProvider } from './providers/query.jsx'
 import { router } from './providers/router'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router}/>
+    <QueryProvider>
+      <RouterProvider router={router}/>
+    </QueryProvider>
   </StrictMode>,
 )
