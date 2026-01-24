e125ab6bfbcc6bf3fb354b5d5b3dfa03275de3d7
Guilherme Godoy <guilherme.santanaa06@gmail.com>
2026-01-23 23:04:07 -0300

    criação de endpoint para listagem de empresas

---
+++ src/entities/company/api/api.js
@@ -0,0 +1,11 @@
+import { supabase } from '@shared/api/supabaseClient';
+
+export async function getCompaniesSummary() {
+    const { data, error } = await supabase
+        .from('company')
+        .select('id, trade_name, status, annual_revenue, company_contact( name )')
+        .eq('company_contact.is_main_contact', true)
+
+    if (error) throw new Error(error.message);
+    return data;
+};
