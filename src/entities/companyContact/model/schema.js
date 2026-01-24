8922ed13fb110d1b42a56e2a3f48a31f383e503e
Guilherme Godoy <guilherme.santanaa06@gmail.com>
2026-01-23 23:02:10 -0300

    Criação do esquema de funcionario da empresa

---
+++ src/entities/companyContact/model/schema.js
@@ -0,0 +1,18 @@
+import { z } from 'zod';
+
+export const companyContactSchema = z.object({
+    id: z.uuid(),
+    name: z.string().min(2).max(100),
+    email: z.email().nullable(),
+    phone: z.string().min(10).max(15).nullable(),
+    role: z.string().min(2).max(50),
+    is_main_contact: z.boolean().default(false),
+    created_at: z.string(),
+});
+
+export const createContactSchema = companyContactSchema.omit({
+    id: true,
+    created_at: true,
+});
+
+export const updateContactSchema = createContactSchema.partial();
