f4e1b99b3c71ffb1cc29b86efa6df41de4acd146
Guilherme Godoy <guilherme.santanaa06@gmail.com>
2026-01-24 22:02:10 -0300

    Adicinando estilização de colunas via prop

--- src/shared/ui/Table.jsx
+++ src/shared/ui/Table.jsx
@@ -1,23 +1,37 @@
 export function Table({ columns, data }) {
     return (
-        <table className="border-collapse shadow-2xl w-full [&_th]:p-1 [&_td]:py-2 [&_td]:px-10 [&_tr]:text-content-base">
+        <table className="rounded-md border-collapse shadow-2xl w-full [&_th]:p-1 [&_td]:py-2 [&_td]:px-10 [&_tr]:text-content-base">
             <thead className="text-content-base bg-bg-card border-b-2 border-border font-medium [&_th]:text-brand-primary">
                 <tr>
                     {columns.map((column) => (
-                        <th key={column.label}>{column.label}</th>
+                        <th className="first:rounded-tl-md last:rounded-tr-md" key={column.label}>{column.label}</th>
                     ))}
                 </tr>
             </thead>
             <tbody className="bg-bg-card [&_td]:text-sm [&_tr:hover]:text-hover-text [&_tr:nth-child(odd)]:bg-card [&_tr:hover]:bg-blue-500/10 [&_td]:border-t [&_td]:border-border">
-                {data.map((row, rowIndex) => (
-                    <tr key={rowIndex}>
-                        {columns.map((column) => (
-                            <td key={column.label}>
-                                {row[column.accessor]}
-                            </td>
-                        ))}
+                {
+                data && data.length === 0 ? (
+                    <tr>
+                        <td className="text-center p-5" colSpan={columns.length}>Nenhum dado disponível.</td>
                     </tr>
-                ))}
+                ) :
+                (data.map((row, rowIndex) => (
+                    <tr key={rowIndex}>
+                        {columns.map((column, index) => {
+                            const isLastRow = rowIndex === data.length - 1;
+                            const isLastColumn = index === columns.length - 1;
+
+                            const style = isLastRow && index === 0 ? "rounded-bl-md" : isLastRow && isLastColumn ? "rounded-br-md" : "";
+
+                            return (
+                                <td className={style + ' ' + column.className} key={column.label}>
+                                    {column.render ? column.render(row[column.key]) : row[column.key]}
+                                </td>
+                            );
+                        })}
+                    </tr>
+                )))}
+
             </tbody>
         </table>
     );
