23ad866bd6d33fedd44ba9a8adce232ccacdd054
Guilherme Godoy <guilherme.santanaa06@gmail.com>
2026-02-01 11:24:26 -0300

    Criando skeleton do card

--- src/shared/ui/card/Card.jsx
+++ src/shared/ui/card/Card.jsx
@@ -1,7 +1,17 @@
+
+const CARD_STYLE = "bg-bg-card p-2 border border-border rounded-md shadow-sm";
+
 export function Card({ children, className }) {
     return (
-        <div className={`bg-bg-card p-2 border border-border rounded-md shadow-sm ${className}`}>
+        <div className={CARD_STYLE + " " + className}>
             {children}
+        </div>
+    );
+};
+
+export function CardSkeleton() {
+    return (
+        <div className={CARD_STYLE + " bg-gray-100 dark:bg-gray-700 animate-pulse"}>
         </div>
     );
 };
