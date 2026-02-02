43ea340d6dbf77fff77d4b8fb1a75311649e7f4f
Guilherme Godoy <guilherme.santanaa06@gmail.com>
2026-02-02 18:30:55 -0300

    removendo contexto do modalConfirm

--- src/app/main.jsx
+++ src/app/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { QueryProvider } from './providers/query.jsx'
-import { ModalConfirmProvider } from './providers/modal.jsx'
 import { ToastProvider } from './providers/toast.jsx'
 import { router } from './providers/router'
 import './index.css'
@@ -10,11 +9,9 @@ import './index.css'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <QueryProvider>
-      <ModalConfirmProvider>
-        <ToastProvider>
-          <RouterProvider router={router}/>
-        </ToastProvider>
-      </ModalConfirmProvider>
+      <ToastProvider>
+        <RouterProvider router={router}/>
+      </ToastProvider>
     </QueryProvider>
   </StrictMode>,
 )
