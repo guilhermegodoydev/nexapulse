54cca69ddfad0d6cb9868fb2eaa0b7f0ac674c68
Guilherme Godoy <guilherme.santanaa06@gmail.com>
2026-02-02 00:50:19 -0300

    Adicionei um componente de entrada e aprimorei o componente Modal com a propriedade className.

---
+++ src/shared/ui/Input.jsx
@@ -0,0 +1,19 @@
+import { forwardRef } from "react";
+
+export const Input = forwardRef(({ id, label, props, errorMessage, ...rest}, ref) => {
+    return (
+        <div className="flex flex-col relative justify-center">
+            <label htmlFor={id}>{label}</label>
+            <input 
+                id={id} 
+                {...props}
+                ref={ref}
+                className="h-10 rounded-md bg-gray-50 pl-3 focus:outline-brand-primary shadow" 
+                {...rest}
+            />
+            {errorMessage ? <p className="text-red-500 text-xs mt-1">{errorMessage}</p> : null}
+        </div>
+    );
+});
+
+Input.displayName = "Input";
--- src/shared/ui/modal/Modal.jsx
+++ src/shared/ui/modal/Modal.jsx
@@ -1,7 +1,7 @@
 import { X } from "lucide-react";
 import { createPortal } from "react-dom";
 
-export function Modal({ isOpen, title, onClose, children}) {
+export function Modal({ isOpen, title, onClose, children, className}) {
     if (!isOpen) return null;
 
     const portalRoot = document.getElementById('modal-root');
@@ -13,7 +13,7 @@ export function Modal({ isOpen, title, onClose, children}) {
 
     return createPortal(
         <div className="fixed z-3 backdrop-blur-xs inset-0 bg-black/70 bg-opacity-50 flex items-center justify-center">
-            <div className="bg-white p-6 rounded-lg shadow-lg">
+            <div className={`bg-white p-6 rounded-lg shadow-lg ${className}`}>
                 <div className="flex items-center justify-between mb-4">
                     <h2 className="text-xl font-bold">{title}</h2>
                     <button aria-label="Fechar Modal" className="cursor-pointer" onClick={onClose}>
