1c14d6382542f4b7a8bbc66e214a8888857fed31
Guilherme Godoy <guilherme.santanaa06@gmail.com>
2026-02-02 18:31:52 -0300

    Alterando controle sobre modal para o botão de deletar

--- src/features/companyDelete/ui/DeleteCompanyButton.jsx
+++ src/features/companyDelete/ui/DeleteCompanyButton.jsx
@@ -1,22 +1,24 @@
 import { Trash2 } from "lucide-react";
-import { useDeleteCompany } from "@entities/company/model/hooks";
-import { useConfirmModal } from "@app/providers/modal";
-import { Button } from "@shared/ui/Button";
+import { useState } from "react";
 import { toast } from "sonner";
 
+import { useDeleteCompany } from "@entities/company/model/hooks";
+
+import { Button } from "@shared/ui/Button";
+import { ModalConfirm } from "@shared/ui/modal/ModalConfirm";
+
 export function DeleteCompanyButton ({ companyId, companyName }) {
-    const { openModal, closeModal } = useConfirmModal();
+    const [ modalIsOpen, setModalIsOpen ] = useState(false);
     const { mutate, isPending } = useDeleteCompany();
 
     const handleDelete = () => {
+        setModalIsOpen(false);
         mutate(companyId, {
-            onSuccess: () => {
+            onSuccess: () => { 
                 toast.success(`Empresa ${companyName} excluída com sucesso!`);
-                closeModal();
             },
             onError: () => {
                 toast.error(`Erro ao excluir a empresa ${companyName}. Tente novamente mais tarde.`);
-                closeModal();
             }
         });
     };
@@ -24,9 +26,17 @@ export function DeleteCompanyButton ({ companyId, companyName }) {
     return (
         <>
             <Button
-                onClick={() => openModal('Confirmar Exclusão', `Tem certeza que deseja excluir ${companyName}?`, handleDelete)}   
+                onClick={() => setModalIsOpen(true)}   
                 isLoading={isPending}
                 renderItem={() => <Trash2 />}
+            />
+
+            <ModalConfirm 
+                title="Confirmar Exclusão" 
+                description={`Tem certeza que deseja excluir ${companyName}`}
+                isOpen={modalIsOpen}
+                onConfirm={handleDelete}
+                onCancel={() => setModalIsOpen(false)}
             />
         </>
     );
