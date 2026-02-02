e64b1825d0c4ccc8dd25302431b382084fab22f6
Guilherme Godoy <guilherme.santanaa06@gmail.com>
2026-02-02 17:06:55 -0300

    Formatando datas para formato pt-BR

--- src/entities/company/model/schema.js
+++ src/entities/company/model/schema.js
@@ -49,6 +49,6 @@ export const companySummarySchema = companySchema.pick({
     industry: company.industry,
     lifecycleStage: company.lifecycle_stage.charAt(0).toUpperCase() + company.lifecycle_stage.slice(1).toLowerCase(),
     revenue: new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL', notation: 'compact', maximumFractionDigits: 1}).format(company.annual_revenue) || 0,
-    lastContact: company.company_contact?.[0]?.last_contact || 'Não informado',
+    lastContact: new Date(company.company_contact?.[0]?.last_contact + 'T00:00:00').toLocaleDateString() || 'Não informado',
     mainContactName: company.company_contact?.[0]?.name || 'Não informado',
 }));
