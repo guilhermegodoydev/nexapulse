c7597ebf8b61c781f3b293cc158b81f154c3077e
Guilherme Godoy <guilherme.santanaa06@gmail.com>
2026-03-01 17:28:21 -0300

    refatorando hooks para incluir validação de sessão e tratamento de erros

--- src/entities/company/model/hooks.js
+++ src/entities/company/model/hooks.js
@@ -1,42 +1,64 @@
-import { useQuery } from '@tanstack/react-query';
-import { getCompaniesSummary, getCompaniesStat, getCompanyMinimal } from '../api/api';
-import { companyMinimalFormSchema, companySummarySchema } from '../model/schema';
-import { companiesStatSchema } from '../model/statSchema';
-import { z } from 'zod';
+import { useQuery } from "@tanstack/react-query";
+import { getCompaniesSummary, getCompaniesStat, getCompanyMinimal } from "../api/api";
+import { companyMinimalFormSchema, companySummarySchema } from "../model/schema";
+import { companiesStatSchema } from "../model/statSchema";
+import { z } from "zod";
+import { useSession } from "@entities/user/model/sessionContext";
 
 export function useCompaniesSummary(page = 0, pageSize = 25, search) {
-    return useQuery({
-        queryKey: ['companiesSummary', { page, pageSize, search }],
-        queryFn: async () => {
-            const { data, total } = await getCompaniesSummary(page, pageSize, search);
-            const validate = z.array(companySummarySchema).parse(data);
-            return { rows: validate, total };
-        },
-        staleTime: 5 * 60 * 1000,
-        placeholderData: (prev) => prev,
-    });
+  const { session } = useSession();
+
+  return useQuery({
+    queryKey: ["companiesSummary", { page, pageSize, search }, session?.user?.id],
+    queryFn: async () => {
+      const { data, total } = await getCompaniesSummary(page, pageSize, search);
+      const validate = z.array(companySummarySchema).parse(data);
+      return { rows: validate, total };
+    },
+    enabled: !!session,
+    staleTime: 5 * 60 * 1000,
+    placeholderData: (prev) => prev,
+  });
 }
 
 export function useCompaniesStat() {
-    return useQuery({
-        queryKey: ['companiesStat'],
-        queryFn: async () => {
+  const { session } = useSession();
+
+  return useQuery({
+    queryKey: ["companiesStat", session?.user?.id],
+    queryFn: async () => {
+        try {
             const data = await getCompaniesStat();
             return companiesStatSchema.parse(data);
-        },
-        staleTime: 5 * 60 * 1000
-    });
+        } catch (error) {
+            console.error('Error ao carregar estatísticas das empresas', error);
+            throw new Error(
+                error?.message || 'Erro ao carregar estatísticas das empresas. Tente novamente mais tarde.'
+            );
+        }
+    },
+    enabled: !!session,
+    staleTime: 5 * 60 * 1000,
+  });
 }
 
 export function useCompanyMinimal(companyId) {
-    return useQuery({
-        queryKey: ['companyBasic', companyId],
-        queryFn: async () => {
+  return useQuery({
+    queryKey: ["companyBasic", companyId],
+    queryFn: async () => {
+        try {
             const data = await getCompanyMinimal(companyId);
             const validate = companyMinimalFormSchema.parse(data);
             return validate;
-        },
-        enabled: !!companyId,
-        staleTime: 5 * 60 * 1000
-    });
+        }
+        catch (error) {
+            console.error('Error ao carregar dados da empresa minimal', error);
+            throw new Error(
+                error?.message || 'Erro ao carregar dados da empresa. Tente novamente mais tarde.'
+            );
+        }
+    },
+    enabled: !!companyId,
+    staleTime: 5 * 60 * 1000,
+  });
 }
