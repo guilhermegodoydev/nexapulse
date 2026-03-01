01437dfc939bf2f538a625778fe6b0016c2ffe54
Guilherme Godoy <guilherme.santanaa06@gmail.com>
2026-03-01 17:27:42 -0300

    Adicionando validação de sessão

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,9 +1,15 @@
 import { Outlet } from "react-router-dom";
 import { SideBar } from "@widgets/sidebar/ui/SideBar";
-import { useMediaQuery } from "@shared/lib/useMediaQuery";
+import { useMediaQuery } from "@shared/lib/useMediaQuery";        
+import { useSession } from "@entities/user/model/sessionContext"; 
 
 export default function App() {
+  const { session, isInitialized } = useSession();
   const isMobile = useMediaQuery("(max-width: 767px)");
+
+  if (!isInitialized) return <p>Carregando...</p>;
+
+  if (!session) return null;
 
   return (
     <div>
