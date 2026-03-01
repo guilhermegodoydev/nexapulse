218dd759622b656b00a15dffb85e3445b064c0b4
Guilherme Godoy <guilherme.santanaa06@gmail.com>
2026-03-01 20:36:40 -0300

    corrigindo caminho da imagem do logo na barra lateral

--- src/widgets/sidebar/ui/SideBarHeader.jsx
+++ src/widgets/sidebar/ui/SideBarHeader.jsx
@@ -6,7 +6,7 @@ export function SideBarHeader({ isOpen, isMobile, onClose}) {
         <div className="flex mb-10 gap-4 lg:gap-0 overflow-hidden transition-all duration-500 ease-in-out group-hover:gap-4">
             {isOpen && isMobile
                 ? <button aria-label="Fechar menu" onClick={onClose}><Menu/></button>
-                : <img src="public/logo2.svg" alt="NexaPulse Logo" className="w-[55px]"/>
+                : <img src="/logo2.svg" alt="NexaPulse Logo" className="w-[55px]"/>
             }
             <h1 className="text-brand-primary sideBarItemAnimation">NexaPulse</h1>
         </div>
