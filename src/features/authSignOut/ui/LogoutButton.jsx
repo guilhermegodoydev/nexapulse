266c7afd6c74a1853d1c42228eab9d15737d458a
Guilherme Godoy <guilherme.santanaa06@gmail.com>
2026-03-01 12:14:16 -0300

    Adicionando spnnier de carregamento

--- src/features/authSignOut/ui/LogoutButton.jsx
+++ src/features/authSignOut/ui/LogoutButton.jsx
@@ -1,4 +1,3 @@
-import { Button } from "@shared/ui/Button";
 import { useSignOut } from "@features/authSignOut/model/useSignOut";
 import { LogOut } from "lucide-react";
 
@@ -6,17 +5,13 @@ export function LogoutButton() {
     const { mutate, isPending } = useSignOut();
 
     return (
-        <Button 
-            label="Sair" 
-            onClick={() => mutate()}
-            renderItem={() => (
-                <>
-                    <LogOut className="text-content-base rotate-180"/>
-                    <p className="text-content-base font-semibold sideBarItemAnimation">Sair</p>
-                </>
-            )}
-            isLoading={isPending}
-            className="flex gap-4 p-4 rounded-md hover:bg-blue-500/10 lg:gap-0 lg:overflow-hidden hover:bg-[#f6f4f] rounded-md transition-all duration-500 ease-in-out group-hover:gap-4 w-full cursor-pointer"
-        />
+        <button onClick={() => mutate()} className="flex items-center gap-4 p-4 rounded-md hover:bg-blue-500/10 lg:gap-0 lg:overflow-hidden hover:bg-[#f6f4f] rounded-md transition-all duration-500 ease-in-out group-hover:gap-4 w-full cursor-pointer">
+            {isPending ?
+                <div className="h-5 w-5 border-3 border-gray-300 border-t-brand-primary animate-spin rounded-full"></div>
+                :
+                <LogOut className="text-content-base rotate-180"/>
+            }
+            <p className="text-content-base font-semibold sideBarItemAnimation">Sair</p>
+        </button>
     );
 };
