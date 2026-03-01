5c3b08231a1fb9ae782609456f0b21aaf99a6eef
Guilherme Godoy <guilherme.santanaa06@gmail.com>
2026-03-01 01:20:45 -0300

    Movendo lógica de autenticação para features

--- src/features/authByEmail/model/hooks.js
+++ src/features/authByEmail/model/hooks.js
@@ -1,13 +1,17 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { loginWithEmail } from "../api/api";
+import { useNavigate } from "react-router-dom";
 
 export function useSignInWithEmail() {
     const queryClient = useQueryClient();
-    
+    const navigate = useNavigate();
+
     return useMutation({
         mutationFn: async ({ email, password }) => loginWithEmail(email, password),
         onSuccess: (user) => {
             queryClient.setQueryData(['user'], user);
+
+            navigate("/", { replace: true });
         }
     });
 }
--- src/features/authByEmail/ui/AuthByEmail.jsx
+++ src/features/authByEmail/ui/AuthByEmail.jsx
@@ -1,28 +1,20 @@
-import { useSignInWithEmail } from "@entities/user/model/hooks";
+import { useSignInWithEmail } from "../model/hooks";
 import { Input } from "@shared/ui/Input";
 import { Button } from "@shared/ui/Button";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { authByEmailSchema } from "@entities/user/model/schema";
-import { useNavigate } from "react-router-dom";
 
 export function AuthByEmail() {
     const { mutate, isPending } = useSignInWithEmail();
-    const navigate = useNavigate();
     const { register, handleSubmit, formState: { errors } } = useForm({
         resolver: zodResolver(authByEmailSchema),
     });
 
-    const handleLogin = ({ email, password}) => {
-        console.log(email, password);
-        mutate({ email, password }, {
-            onSuccess: () => navigate("/", { replace: true }),
-        });
-    }
 
     return (
         <>
-            <form onSubmit={handleSubmit(handleLogin)} className="bg-card border border-border rounded-lg p-8 w-full max-w-md space-y-6">
+            <form onSubmit={handleSubmit(mutate)} className="bg-card border border-border rounded-lg p-8 w-full max-w-md space-y-6">
                 <h1>Autenticação</h1>
 
                 <Input 
