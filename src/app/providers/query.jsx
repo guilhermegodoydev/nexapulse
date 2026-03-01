15c2dc9f5188ad3215d26f803e536a7b1a84ebf0
Guilherme Godoy <guilherme.santanaa06@gmail.com>
2026-03-01 20:35:08 -0300

    alterando tentativas de request em caso de erro para apenas 1

--- src/app/providers/query.jsx
+++ src/app/providers/query.jsx
@@ -4,6 +4,7 @@ const queryClient = new QueryClient({
     defaultOptions: {
         queries: {
             staleTime: 5 * 60 * 1000,
+            retry: 1,
             refetchOnWindowFocus: false,
         }
     }
