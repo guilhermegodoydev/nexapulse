3d0578ab73c738f9750dee50a71e763dc24d01b1
Guilherme Godoy <guilherme.santanaa06@gmail.com>
2026-04-02 22:10:32 -0300

    Importando toast

--- src/features/companyCreate/model/hooks.js
+++ src/features/companyCreate/model/hooks.js
@@ -1,5 +1,6 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { createCompany } from '../api/api';
+import { toast } from 'sonner';
 
 export function useCreateCompany() {
     const queryClient = useQueryClient();
