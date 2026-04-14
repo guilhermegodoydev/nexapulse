6d0d9dca646de0612f11d4f7e29183e2204049ad
Guilherme <guilherme.santanaa06@gmail.com>
2026-04-14 20:01:30 -0300

    feat: add password visibility toggle with accessibility support

--- src/features/authByEmail/ui/AuthByEmail.jsx
+++ src/features/authByEmail/ui/AuthByEmail.jsx
@@ -1,15 +1,18 @@
+import { useState } from "react";
 import { useSignInWithEmail } from "../model/hooks";
 import { Input } from "@shared/ui/Input";
 import { Button } from "@shared/ui/Button";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { authByEmailSchema } from "../model/schema";
+import { Eye, EyeClosed } from "lucide-react";
 
 export function AuthByEmail() {
     const { mutate, isPending } = useSignInWithEmail();
     const { register, handleSubmit, formState: { errors }, setError } = useForm({
         resolver: zodResolver(authByEmailSchema)
     });
+    const [ passwordIsVisible, setPasswordIsVisible] = useState(false);
 
     const onSubmit = (data) => {
         mutate(data, {
@@ -37,17 +40,33 @@ export function AuthByEmail() {
                     id="email" 
                     errorMessage={errors?.email?.message}
                     autoComplete="email"
+                    aria-invalid={errors?.email ? "true" : "false"}
+                    aria-describedby={errors?.email ? "email-error" : undefined}
                     {...register("email")} 
                 />
                 
-                <Input 
-                    label="Senha" 
-                    id="password" 
-                    type="password" 
-                    errorMessage={errors?.password?.message}
-                    autoComplete="current-password"
-                    {...register("password")} 
-                />
+                <label htmlFor="password">Senha</label>
+                <div className="h-10 relative mb-10">
+                    <input 
+                        id="password" 
+                        autoComplete="current-password"
+                        type={passwordIsVisible ? "text" : "password"} 
+                        aria-invalid={errors?.password ? "true" : "false"}
+                        aria-describedby={errors?.password ? "password-error" : undefined}
+                        className={`h-full w-full rounded-md bg-gray-50 dark:bg-gray-700 pl-3 focus:outline-brand-primary shadow pr-10`}
+                        {...register("password")} 
+                    />
+                    <button 
+                        aria-label={passwordIsVisible ? "Ocultar Senha" : "Mostrar Senha"}
+                        aria-controls="password"
+                        type="button" 
+                        className="absolute top-1/2 right-2 transform -translate-y-1/2" 
+                        onClick={() => setPasswordIsVisible(!passwordIsVisible)} 
+                    >
+                        {passwordIsVisible ? <Eye size={20}/> : <EyeClosed size={20}/>}
+                    </button>
+                    <p className="text-red-500 text-xs">{errors?.password?.message}</p>
+                </div>
 
                 {errors.root && (<p className="text-red-500 text-sm font-medium">{errors.root.message}</p>)}
 
